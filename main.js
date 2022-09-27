ad6ab2a3ac7d869d1797d0f64efb0ec57dda27f0
shyamtala003 <shyamtala003@gmail.com>
2022-09-27 12:59:46 +0530

    V.2.0

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
 let quoteDiv = document.getElementById('quote');
-let author = document.getElementById('autor');
+let author = document.getElementById('author');
+let TwitterLink;
 
-let generateQuotes = async() => {
+let generateQuotes = async () => {
     let quote = await fetch("https://api.quotable.io/random");
     let result = await quote.json();
-    quoteDiv.textContent=result.content;
-    author.textContent=`- ${result.author}`;
+    quoteDiv.textContent = result.content;
+    author.textContent = `- ${result.author}`;
 
-    // let image = await fetch("https://api.unsplash.com/photos/random/?client_id=MW3gI7eSJgNQNf2qdvH4SiA9yJESUcZJjlTgEtDVl2c");
-    // let imageResult= await image.json();
-    // document.body.style.backgroundImage=`url(${imageResult.links.download})`;
+    // code for twitte
+    let twitterText = `${result.content} \n-- ${result.author}`
+
+    TwitterLink = `https://twitter.com/intent/tweet?text=${twitterText}`
+
+    let twitterBtn = document.getElementById('twitter__btn');
+    twitterBtn.setAttribute("href", TwitterLink);
 
 }
 
@@ -17,19 +22,30 @@ generateQuotes();
 
 
 // text to speech
-let textToSpeech= ()=>{
+let textToSpeech = () => {
     let quote = document.getElementById('quote').textContent;
+    quote += "\tWritten by" + document.getElementById('author').textContent;
     let msg = new SpeechSynthesisUtterance(quote);
     speechSynthesis.speak(msg);
 }
 
 // copy to clipboard
 
-function copyToClipboard() {
-    let quote = document.getElementById('quote').textContent;
-    quote.select();
-    quote.setSelectionRange(0, 99999); // For mobile devices
-  
-     // Copy the text inside the text field
-    navigator.clipboard.writeText(quote);
-  }
+var clipboard = new ClipboardJS('.copy__to_clipboard');
+
+clipboard.on('success', function (e) {
+    let alertMsg = document.querySelector("#msg");
+    alertMsg.classList.remove("hideMsg");
+    alertMsg.classList.add("showMsg");
+    setTimeout(() => {
+        alertMsg.classList.add("hideMsg");
+        alertMsg.classList.remove("showMsg");
+    }, 1500)
+
+    e.clearSelection();
+});
+
+clipboard.on('error', function (e) {
+    console.error('Action:', e.action);
+    console.error('Trigger:', e.trigger);
+});
